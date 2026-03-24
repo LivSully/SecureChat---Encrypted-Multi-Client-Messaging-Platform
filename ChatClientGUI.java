46dbd6c6450ccdedf68d15b70b6a62d0a067502f
LivSully <livybeanbean@gmail.com>
2026-03-24 11:07:39 -0400

    test

--- ChatClientGUI.java
+++ ChatClientGUI.java
@@ -1,3 +1,5 @@
+
+//this one
 import java.io.IOException;
 
 import javax.swing.*;
@@ -9,10 +11,11 @@ public class ChatClientGUI extends JFrame {
     private CHAT_CLIENT_TEST client;
     private JButton sendButton;
     private JButton clearButton;
+
     public ChatClientGUI() {
 
         ImageIcon logo = new ImageIcon("CoolSecureChatLogo.png");
-        //Image img = logo.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+        // Image img = logo.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
         JLabel lblLogo = new JLabel(logo);
         lblLogo.setHorizontalAlignment(JLabel.CENTER);
         add(lblLogo, BorderLayout.NORTH);
@@ -28,27 +31,28 @@ public class ChatClientGUI extends JFrame {
             inputField.setText("");
         });
         sendButton.addActionListener(e -> {
-           String msg = inputField.getText();
-           client.sendMessage(msg);
-           inputField.setText(""); 
+            String msg = inputField.getText();
+            client.sendMessage(msg);
+            inputField.setText("");
         });
         clearButton.addActionListener(e -> {
             chatArea.setText("");
         });
         add(new JScrollPane(chatArea), BorderLayout.CENTER);
-        //add(inputField, BorderLayout.SOUTH);
+        // add(inputField, BorderLayout.SOUTH);
         JPanel bottomPanel = new JPanel(new BorderLayout());
         JPanel buttonPanel = new JPanel();
-        //JPanel topPanel = new JPanel(new BorderLayout());
+        // JPanel topPanel = new JPanel(new BorderLayout());
         buttonPanel.add(sendButton);
         buttonPanel.add(clearButton);
-        //topPanel.add(lblLogo);
+        // topPanel.add(lblLogo);
         bottomPanel.add(inputField, BorderLayout.CENTER);
         bottomPanel.add(buttonPanel, BorderLayout.EAST);
         add(bottomPanel, BorderLayout.SOUTH);
         setSize(400, 500);
         setVisible(true);
     }
+
     public void connect(String host, int port) throws IOException {
         client = new CHAT_CLIENT_TEST(host, port, this);
     }
